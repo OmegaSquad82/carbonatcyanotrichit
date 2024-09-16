71ef2a37d5b5849c0a130a35fe438e138b53459b
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-09-16 22:22:56 +0200

    Rebase to Bazzite Deck Stable

--- build.sh
+++ build.sh
@@ -6,18 +6,20 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
+rpm-ostree install \
+    bpytop \
+    byobu \
+    cowsay \
+    goverlay \
+    htop \
+    kitty \
+    mangohud \
+    neovim \
+    netdata \
+    powertop \
+    tlp
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
+### Enable additional services
 systemctl enable podman.socket
+systemctl enable sshd.service
+systemctl disable tailscaled.service
